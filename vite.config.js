359f4cae1331829bfb0bad7a9626cfb21b20fab6
minee0505 <wodeyue97@gmail.com>
2025-10-28 14:30:22 +0900

    TRAIN-106 ci: 프론트엔드 S3 배포 및 클라우드플랫폼 설정 추가

--- vite.config.js
+++ vite.config.js
@@ -17,6 +17,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  server: {
+    port: 5050,
+  },
   test: {
     projects: [{
       extends: true,
